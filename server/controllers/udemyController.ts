c44aaeb6e0dcf6c0bef03d0cc951820deb1d3ce3
Jun <junealee07@gmail.com>
2023-07-10 16:30:11 -0700

    filled out udemyController but missing query strings

--- server/controllers/udemyController.ts
+++ server/controllers/udemyController.ts
@@ -1,1 +1,27 @@
 const udemyController = {};
+
+udemyController.getCourse = async (req, res, next) => {
+  const { userInput } = req.body;
+  if (!userInput)
+    return next({
+      log: 'Missing userInput!!',
+      status: 400,
+      message: { err: 'Missing userInput!!' },
+    });
+  const queryStr;
+  const addQueryStr;
+  const result = await db.query(queryStr);
+  try {
+    if (result) {
+      res.locals.url = result.rows[0];
+    } else {
+      const postResult = await db.query(addQueryStr);
+      const fetchResult = await db.query(queryStr);
+      res.locals.url = fetchResult.rows[0];
+    }
+  } catch (err) {
+    next({
+      log: 'Error in udemyController.getCourse',
+    });
+  }
+};
