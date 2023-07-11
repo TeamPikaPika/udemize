ac8ce64de1a7bddbf9bbdce15d156a540c3018d6
Jun <junealee07@gmail.com>
2023-07-10 17:09:57 -0700

    built out routers

---
+++ server/routes/udemyApi.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import udemyController from '../controllers/udemyController';
+
+const udemyRouter = Router();
+
+udemyRouter.get('/', (req, res) => {});
+
+export default udemyRouter;
