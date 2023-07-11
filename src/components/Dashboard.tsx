400a1024d3da23616e76b421f470151dfc3dea37
Steve Lemlek <stevelemlek@gmail.com>
2023-07-11 08:34:22 -0500

    feat: TechTable component added, Dashboard formData updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,44 +1,56 @@
-import { FC, useState } from 'react'
+import { FC, useState, SyntheticEvent } from 'react';
+import TechTable from './TechTable';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Typography } from '@mui/material'
-
+import { Typography } from '@mui/material';
 
 const Dashboard: FC = () => {
+	//state to store users' input
+	const [formData, setFormData] = useState('');
 
-  //state to store users' input
-  const [formData, setFormData] = useState('')
+	//save current input in state, before user submits
+	// const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFormData(e.target.value);
+	// };
 
-  //save current input in state, before user submits
-  const onChange = (e: any )=> {
-    setFormData(e.target.value)
-  }
+	//handle what happens to input when user submits
+	const onSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		if (!formData) return;
+		//TODO: insert call to backend
+    //TODO: update the user's saved techstack with the new tech
+		//reset formData
+		setFormData('');
+	};
 
-  //handle what happens to input when user submits
-  const onSubmit = (e: any) => {
-    e.preventDefault()
-    if (!formData) return;
-    //insert call to backend
-    //reset formData
-    setFormData('')
 
-  }
+	return (
+		<>
+			<Typography variant="h2" sx={{ textAlign: 'center' }}>
+				Udemize
+			</Typography>
+			<Box
+				className="dashboard"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					width: '50%',
+					margin: '0 auto',
+				}}
+			>
+				<TextField
+					id="outlined-basic"
+					label="new tech"
+					variant="outlined"
+					sx={{ width: '100%' }}
+					onChange={(e) => setFormData(e.target.value)}
+				/>
+			</Box>
+			<TechTable />
+		</>
+	);
+};
 
-  return (
-    <>
-    <Typography variant = "h2" sx = {{textAlign: 'center'}}>
-      Udemize
-    </Typography>
-    <Box className="dashboard" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '50%', margin: '0 auto'}}>
-      <TextField
-        id="outlined-basic"
-        label="new tech"
-        variant="outlined"
-        sx = {{width: "100%"}}
-      />
-    </Box>
-    </>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
---
+++ src/components/TechTable.tsx
@@ -0,0 +1,80 @@
+import { FC } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const TechTable: FC = () => {
+	//placeholder for the user's saved tech, which we'll get from redux or usecontext
+	const rows = [
+		{
+			techName: 'Angular',
+			threePros: {
+				1: 'sample pro',
+				2: 'sample pro',
+				3: 'sample pro',
+			},
+			threeCons: {
+				1: 'sample con',
+				2: 'sample con',
+				3: 'sample con',
+			},
+			threeAlternatives: {
+				1: 'React',
+				2: 'Vue',
+				3: 'Polymer',
+			},
+			threeUdemy: {
+				1: {
+					link: '',
+					title: '',
+					thumbnail: '',
+				},
+				2: {
+					link: '',
+					title: '',
+					thumbnail: '',
+				},
+				3: {
+					link: '',
+					title: '',
+					thumbnail: '',
+				},
+			},
+		},
+	];
+
+	//create a row for our table for every object in the rows constant
+	const rowElements = rows.map((ele) => (
+		<TableRow
+			key={ele.techName}
+			sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+		>
+			<TableCell component="th" scope="row">
+				{ele.techName}
+			</TableCell>
+		</TableRow>
+  ));
+  
+  console.log(rows, rowElements)
+
+	return (
+		<TableContainer component={Paper} sx = {{width: "50%", margin: "2em auto"}}>
+			<Table sx={{ width: "50%" }} aria-label="simple-table">
+				{/* <TableHead>
+					<TableRow>
+						<TableCell>Tech Name</TableCell>
+					</TableRow>
+				</TableHead> */}
+        <TableBody>
+          {rowElements}
+        </TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
+
+export default TechTable;
