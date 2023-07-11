33ee57198eee668566077cb6a1141ba932290f84
Jun <junealee07@gmail.com>
2023-07-10 19:43:03 -0700

    posting to chatgpt api

--- server/actions/getUdemyLink.ts
+++ server/actions/getUdemyLink.ts
@@ -1,18 +1,18 @@
-export const getUdemyLink = async (userInput: string) => {
-  try {
-    const response = await fetch(
-      `https://api.udemy.com/v1/courses?search=${encodeURIComponent(
-        userInput
-      )}`,
-      {
-        headers: {
-          Authorization: 'Bearer YOUR_ACCESS_TOKEN', // Replace with access token
-        },
-      }
-    );
-    const processedResponse = await response.json();
-    return processedResponse;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// export const getUdemyLink = async (userInput: string) => {
+//   try {
+//     const response = await fetch(
+//       `https://api.udemy.com/v1/courses?search=${encodeURIComponent(
+//         userInput
+//       )}`,
+//       {
+//         headers: {
+//           Authorization: 'Bearer YOUR_ACCESS_TOKEN', // Replace with access token
+//         },
+//       }
+//     );
+//     const processedResponse = await response.json();
+//     return processedResponse;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
--- server/routes/chatApi.ts
+++ server/routes/chatApi.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import gptController from '../controllers/gptController';
 
 const chatRouter = Router();
 
-chatRouter.get('/', (req, res) => {});
+chatRouter.get('/', gptController.getProsCons, (req, res) => {
+  return res.status(200).json(res.locals.prosCons);
+});
 
 export default chatRouter;
