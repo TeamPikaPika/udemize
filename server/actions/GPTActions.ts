7dcef0238d1cd026a3a661c595dd61fcc38dbcc6
Kasu <kasu@kasu-it.dev>
2023-07-12 11:09:01 -0500

    feat: add summary from GPT

--- server/actions/GPTActions.ts
+++ server/actions/GPTActions.ts
@@ -7,14 +7,14 @@ const openai = new OpenAIApi(configuration);
 
 const GPTActions: any = {}
 
-GPTActions.getPros = async (userInput: string): Promise<string> => {
+GPTActions.prompt = async (proompt: string): Promise<string> => {
   try {
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'user',
-          content: `give me 3 pros of the tech ${userInput} where each point in no more that 250 characters including whitespace I cannot hold more characters that that in my database.`,
+          content: proompt,
         },
       ],
     });
@@ -31,30 +31,5 @@ GPTActions.getPros = async (userInput: string): Promise<string> => {
     }
   }
 };
-
-GPTActions.getCons = async (userInput: string): Promise<string> => {
-  try {
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: `give me 3 cons of the tech ${userInput} where each point in no more that 250 characters including whitespace I cannot hold more characters that that in my database.`,
-        },
-      ],
-    });
-    console.log(completion.data.choices[0].message.content)
-    return completion.data.choices[0].message.content;
-  } catch (err) {
-    if (err.response) {
-      console.log(err.response.status);
-      console.log(err.response.data);
-      return err;
-    } else {
-      console.log(err.message);
-      return err;
-    }
-  }
-}
 
 export default GPTActions;
