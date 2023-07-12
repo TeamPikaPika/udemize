c91d512c989677378d3b10f86daa9dbbde72d5c8
Steve Lemlek <stevelemlek@gmail.com>
2023-07-11 19:22:59 -0500

    feat: changing loggedIn state pushes to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,8 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoutes from './ProtectedRoutes';
-import {
-	createBrowserRouter,
-	RouterProvider,
-	BrowserRouter,
-	Route,
-	Routes,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserProvider from './context/UserContext';
 
 /*
 this is the suggested way to make a router, but wouldn't work with protected routes
@@ -47,19 +42,20 @@ this is the suggested way to make a router, but wouldn't work with protected rou
 //   }
 // ])
 
-
 const App: FC = () => {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Container />}>
-					<Route path="/" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
-					<Route element = {<ProtectedRoutes/>}>
-						<Route path="dashboard" element={<Dashboard />} />
+			<UserProvider>
+				<Routes>
+					<Route path="/" element={<Container />}>
+						<Route path="/" element={<Login />} />
+						<Route path="signup" element={<Signup />} />
+						<Route element={<ProtectedRoutes />}>
+							<Route path="dashboard" element={<Dashboard />} />
+						</Route>
 					</Route>
-				</Route>
-			</Routes>
+				</Routes>
+			</UserProvider>
 		</BrowserRouter>
 	);
 };
