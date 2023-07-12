1083937649ee0c9a2c0bde6dcf0c737e6761c266
Jun <junealee07@gmail.com>
2023-07-11 17:45:59 -0700

    revert changes

--- server/actions/postProsCons.ts
+++ server/actions/postProsCons.ts
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 const key = process.env.GPT_KEY;
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,55 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: '/src/index.tsx',
-  mode: "production",
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			// {
-			//   test: /\.tsx?/,
-			//   exclude: /node_modules/,
-			//   use: {
-			//     loader: "babel-loader",
-			//     options: {
-			//       presets: ['@babel/preset-env', '@babel/preset-react'],
-			//     }
-			//   }
-			// },
-		],
+  entry: '/src/index.tsx',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // {
+      //   test: /\.tsx?/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       presets: ['@babel/preset-env', '@babel/preset-react'],
+      //     }
+      //   }
+      // },
+    ],
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	devServer: {
-		hot: true,
-		open: true,
-		historyApiFallback: true,
-		proxy: {
-			'/': 'http://localhost:3000',
-			secure: true,
-			changeOrigin: true,
-		},
-	},
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    proxy: {
+      '/': 'http://localhost:3000',
+      secure: true,
+      changeOrigin: true,
+    },
+  },
 };
