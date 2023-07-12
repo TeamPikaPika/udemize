275ced8bdafae8fa33e4df465689c07e714aa613
Kasu <kasu@kasu-it.dev>
2023-07-12 11:41:20 -0500

    refactor: refactor to initialize data before running it through GPT controller

---
+++ server/controllers/initializeData.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import { InitializeData } from '../types';
+
+const initializeData: InitializeData = {
+  initialize: (req, res, next) => {
+    const { userInput } = req.body
+
+    res.locals.data = { title: userInput.toLowerCase() };
+
+    return next()
+  }
+}
+
+export default initializeData;
